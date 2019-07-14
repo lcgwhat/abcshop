8b5ce9046e42ea9e204e86804a4e21bfc0e8a8fb
lcgwhat <348437207@qq.com>
2019-07-14 16:40:45 +0800

    初始化

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class UserAddress extends Model
+{
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+    public function attributes()
+    {
+        return [
+            'province'      => '省',
+            'city'          => '城市',
+            'district'      => '地区',
+            'address'       => '详细地址',
+            'zip'           => '邮编',
+            'contact_name'  => '姓名',
+            'contact_phone' => '电话',
+        ];
+    }
+    protected $dates = ['last_used_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+    }
+
+    public function own(User $user, UserAddress $address)
+    {
+        return $address->user_id == $user->id;
+    }
+
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            copy right 348437207@qq.com
+        </p>
+        <p class="float-right">
+            <a href="mailto:name@email.com">联系我们</a>
+        </p>
+    </div>
+</footer>
