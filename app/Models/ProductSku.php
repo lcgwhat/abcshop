e3cb29e1e9ef0cc1ac90e35ea0807d96fd33a958
lcgwhat <348437207@qq.com>
2019-07-18 19:53:03 +0800

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\InternalException;
 
 class ProductSku extends Model
 {
@@ -11,5 +12,31 @@ class ProductSku extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * @param $amount
+     * @return mixed
+     * @throws InternalException
+     */
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    /**
+     * @param $amount
+     * @throws InternalException
+     */
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
