75568c69b322c728456758781bd0bec20cd25b1e
lcgwhat <348437207@qq.com>
2019-08-04 10:50:22 +0800

    用户界面-检查优惠卷

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
 
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 Route::get('products', 'ProductsController@index')->name('products.index');
