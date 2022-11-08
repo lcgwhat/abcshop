e54ef3de90f12bbacac7906976daa9a5c2628e06
lcgwhat <348437207@qq,com>
2022-11-08 17:34:31 +0800

    遇到js/css 引入失败，在网络中现实127.0.0.1/js/main.js not found 在， <script src="{{mix('js/app.js')}}"></script>。使用 {{asset('js/app.js')}}">

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title','abc-shop')</title>
     {{--样式--}}
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
 </head>
 <body>
@@ -21,7 +21,7 @@
         @include('layouts._footer')
     </div>
     {{--js 脚本--}}
-    <script src="{{mix('js/app.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
     @yield('scriptsAfterJs')
 </body>
 </html>
